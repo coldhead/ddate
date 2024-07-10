5d5bcf98aa533cc39cea64575fb935817947d58b
Sasha <sasha.p@hush.com>
2024-07-10 16:44:13 +0300

    added tests

---
+++ ddate_test.go
@@ -0,0 +1,37 @@
+package ddate
+
+import (
+	"testing"
+	"time"
+)
+
+func TestFormat(t *testing.T) {
+	want := "Sweetmorn of Chaos, 3136 YOLD"
+	unixStart := time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)
+
+	msg := Format(unixStart)
+	if want != msg {
+		t.Fatalf(`1.1.1970 = %q, want %q`, msg, want)
+	}
+}
+
+func TestIsHolyday(t *testing.T) {
+	want := "Mungday"
+	holy := time.Date(1970, 1, 5, 0, 0, 0, 0, time.UTC)
+
+	celebrate, msg := IsHolyday(holy)
+
+	if want != msg || !celebrate {
+		t.Fatalf(`5.1.1970 must be a %q`, want)
+	}
+}
+
+func TestIsNotHolyday(t *testing.T) {
+	notholy := time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC)
+
+	celebrate, msg := IsHolyday(notholy)
+
+	if celebrate || msg != "" {
+		t.Fatalf(`1.1.1970 was not a holyday`)
+	}
+}
